f272fdeac7a2a868041e472bf7a7ac47f33b0dc2
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-08-09 00:55:24 +0200

    Dashboard: deploy

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -6,7 +6,7 @@ import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
       <Header title='Pages' category='Orders'/>
       <GridComponent id='gridcomp' dataSource={ordersData} allowPaging allowSorting>
         <ColumnsDirective>
